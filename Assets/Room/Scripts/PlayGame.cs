d1de00377728b95a7c7100ddef348f24a27bb284
Lynh Mochi <meloly4@gmail.com>
2021-11-09 16:06:37 -0500

    functional tittle screen

--- Assets/Room/Scripts/PlayGame.cs
+++ Assets/Room/Scripts/PlayGame.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayGame : MonoBehaviour
 {
-    public void Start() {
-        
+    public void Play() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
