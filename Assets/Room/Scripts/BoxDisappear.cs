446f10a2bb94c7fbcb76b511cc8906d1e86ce394
Lynh Mochi <meloly4@gmail.com>
2021-11-18 13:10:59 -0500

    add functional key, box collider to cardboard boxes disappear on click

---
+++ Assets/Room/Scripts/BoxDisappear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxDisappear : MonoBehaviour
+{
+    public GameObject SpiderToSpawn;
+    void Update() {  
+        if (Input.GetMouseButtonDown (0)) {    
+             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+             RaycastHit hit;
+ 
+             if (Physics.Raycast(ray, out hit, 100)) {
+                 if(hit.collider.tag == "cardboardBox") {                         
+                     Debug.Log("---> Hit: ");
+                     Instantiate(SpiderToSpawn,hit.transform);  
+                     Destroy(hit.transform.gameObject);
+                 }
+             }    
+         }
+    }
+}
