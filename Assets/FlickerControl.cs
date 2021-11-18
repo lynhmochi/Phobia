4f49b00bcf3aa35d6546e37070545aa465d0cd36
Mujtaba Hassan <58803814+smmhassan@users.noreply.github.com>
2021-11-18 06:43:04 -0600

    Fixed spider animations, added flickerijg tube lights, a rack for the tools, and something inn the window

---
+++ Assets/FlickerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerControl : MonoBehaviour
+{
+    public bool isFlickering = false;
+    public float timeDelay; 
+    public Material material;
+
+    void Update()
+    {
+        if(isFlickering == false){
+            StartCoroutine(FlickeringLight());
+        }
+    }
+    IEnumerator FlickeringLight(){
+        isFlickering = true;
+        this.gameObject.GetComponent<Light>().enabled = false;
+        material.DisableKeyword ("_EMISSION");
+        timeDelay = Random.Range(0.01f, 0.7f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light>().enabled = true;
+        material.EnableKeyword ("_EMISSION");
+        timeDelay = Random.Range(0.01f, 0.7f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlickering = false;
+    }
+}
